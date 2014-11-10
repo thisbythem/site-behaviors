b44e29645498ca3b43531ced1bc5e976bd864574
James Wagner <james@thisbythem.com>
2014-11-10 11:28:50 -0800

    TOGGLER: Don't prevent default for form elements

--- behaviors/toggler.js
+++ behaviors/toggler.js
@@ -5,13 +5,17 @@
     $el: null,
     $target: null,
     $triggers: null,
+
     targetID: null,
+    elType: null,
 
     init: function ($el) {
       this.$el = $el;
       this.targetID = this.$el.data('toggler-target');
       this.$targets = $('[data-toggler-id~="' + this.targetID + '"]');
       this.$triggers = $('[data-behavior~="toggler"][data-toggler-target~="' + this.targetID + '"]');
+
+      this.setElType();
       this.bindEvents();
     },
 
@@ -19,14 +23,27 @@
       this.$el.on('click', $.proxy(this.onTogglerClick, this));
     },
 
+    setElType: function () {
+      if (this.$el.is('input')) {
+        this.elType = 'input';
+      }
+    },
+
     onTogglerClick: function (e) {
-      e.preventDefault();
+      if (this.shouldPreventDefault()) {
+        e.preventDefault();
+      }
+
       this.$triggers.each(function () {
         $(this).toggleClass('open');
       });
       this.$targets.each(function () {
         $(this).toggleClass('open');
       });
+    },
+
+    shouldPreventDefault: function () {
+      return this.elType !== 'input';
     }
   };
 
